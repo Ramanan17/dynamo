85116b9f9bfc5cb818fd84043097b0221e02f40f
Ramanan17 <34538799+Ramanan17@users.noreply.github.com>
2021-05-15 14:46:48 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Dynamic;
+
+namespace course_hero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "PlayerScore.csv";
+            StreamReader sr = new StreamReader(File.OpenRead(filename));
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadLine();
+            str = sr.ReadLine();
+            while (str != null & str!="")
+            {
+                string[] data = str.Split(",");
+                dynamic player = new ExpandoObject();
+                player.Player = data[0];
+                player.Score = Convert.ToInt32(data[1]);
+                Console.WriteLine($"{player.Player} has a score of {player.Score}");
+                
+                str = sr.ReadLine();
+            }
+            sr.Close();
+        }
+    }
+}
